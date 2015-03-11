a29bbf594eaadffe273c4a14a27f0a6e41a27d65
Roy de Jong <roy@softwarepunt.nl>
2015-03-11 20:11:31 +0100

    Did you know you can use colors in the Chrome console!? Cool!

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    console.log('%cArrr, matey! - Piracy Simulator 2015', 'background: #222; color: #bada55');
+    console.log('%cwww.burningtomato.com / hello@burningtomato.com', 'background: #ff0000; color: #fff');
+
     Game.init();
     Game.start(Settings.TitleMap);
 });
